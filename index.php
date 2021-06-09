1588719eccedf4070b96eef9ba28f43ef8d94840
hubert79 <makula.hubert@gmail.com>
2021-06-09 11:19:50 +0200

    Add files via upload
    skrypt wyszukuje wszystkie tagi a11y

--- index.php
+++ index.php
@@ -35,9 +35,74 @@ if($num != ""){
 }
 
 
+# Podmiot 
+$num = "";
+$num = stripos($htmlContent, "a11y-podmiot");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
+# Url 
+$num = "";
+$num = stripos($htmlContent, "a11y-url");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
 # Data publikacja 
-	$num = stripos($htmlContent, "a11y-data-publikacja");
+$num = "";
+$num = stripos($htmlContent, "a11y-data-publikacja");
 
+if($num != ""){
+	
 	$ca = substr($htmlContent,$num, 1);
 
 	$akt = "";
@@ -61,12 +126,14 @@ if($num != ""){
 	$ile = $k - $s - 1;
 	
 	echo substr($akt, $od, $ile)."<br>";
- 
+}
 
-# Data aktualizacja
+# Data aktualizacja 
+$num = "";
+$num = stripos($htmlContent, "a11y-data-aktualizacja");
 
-	$num = stripos($htmlContent, "a11y-data-aktualizacja");
-
+if($num != ""){
+	
 	$ca = substr($htmlContent,$num, 1);
 
 	$akt = "";
@@ -90,11 +157,15 @@ if($num != ""){
 	$ile = $k - $s - 1;
 	
 	echo substr($akt, $od, $ile)."<br>";
- 
+}
 
 # Status 
-	$num = stripos($htmlContent, "a11y-status");
 
+$num = "";
+$num = stripos($htmlContent, "a11y-status");
+
+if($num != ""){
+	
 	$ca = substr($htmlContent,$num, 1);
 
 	$akt = "";
@@ -118,5 +189,321 @@ if($num != ""){
 	$ile = $k - $s - 1;
 	
 	echo substr($akt, $od, $ile)."<br>";
+}
+
+# Ocena 
+$num = "";
+$num = stripos($htmlContent, "a11y-ocena");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
+# Data sporządzenia 
+$num = "";
+$num = stripos($htmlContent, "a11y-data-sporzadzenie");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
+# Audytor 
+$num = "";
+$num = stripos($htmlContent, "a11y-audytor");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
+# Kontakt 
+$num = "";
+$num = stripos($htmlContent, "a11y-kontakt");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
+# Osoba 
+$num = "";
+$num = stripos($htmlContent, "a11y-osoba");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
+# e-mail
+ 
+$num = "";
+$num = stripos($htmlContent, "a11y-email");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
+# Telefon
+ 
+$num = "";
+$num = stripos($htmlContent, "a11y-telefon");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
+# Procedura
+$num = "";
+$num = stripos($htmlContent, "a11y-procedura");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
+# Architektura 
+$num = "";
+$num = stripos($htmlContent, "a11y-architektura");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
+
+# Aplikacje
+ 
+$num = "";
+$num = stripos($htmlContent, "a11y-aplikacje");
+
+if($num != ""){
+	
+	$ca = substr($htmlContent,$num, 1);
+
+	$akt = "";
+	$flaga = "false";
+
+	while($flaga == "false"){
+		$cut = substr($htmlContent,$num, 1);
+		$akt .= $cut;
+		if($cut == "/" /*chr(60)*/){
+			$flaga = "true";
+		} else {
+			$flaga = "false";
+			$num += 1;
+		}
+	
+	}
+	$s = strpos($akt, ">");
+	$k = strRpos($akt, "<");
+
+	$od = $s + 1;
+	$ile = $k - $s - 1;
+	
+	echo substr($akt, $od, $ile)."<br>";
+}
+
 
 ?>
